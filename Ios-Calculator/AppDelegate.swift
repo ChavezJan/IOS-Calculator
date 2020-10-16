95423fc8e04341f627fc462eaff63b408c01842e
Jorge <chavejoa@gmail.com>
2020-10-16 00:48:24 -0500

    Creating the main view

--- Ios-Calculator/AppDelegate.swift
+++ Ios-Calculator/AppDelegate.swift
@@ -13,8 +13,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Setup
+        setupView()
+
         return true
+    }
+    
+    // MARK: - Private Methods
+    
+    private func setupView() {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let vc = HomeViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+        
+        
     }
 }
 
---
+++ Ios-Calculator/Controller/Home/HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  HomeViewController.swift
+//  Ios-Calculator
+//
+//  Created by Jorge Alejandro Chavez Nuñez on 16/10/20.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    // MARK: - Initialization
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life Cycle
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
