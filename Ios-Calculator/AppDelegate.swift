a556151a203690019808985b19296c5bcadc8b08
Jorge <chavejoa@gmail.com>
2020-10-15 20:16:27 -0500

    Initial commit

---
+++ Ios-Calculator/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  Ios-Calculator
+//
+//  Created by Jorge Alejandro Chavez Nuñez on 15/10/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
